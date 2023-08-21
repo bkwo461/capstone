92da9f36bf01c0c5ac9ccf890e9da2772a47dcd2
Sangjun Min <emesjay97@gmail.com>
2023-08-21 23:18:47 +1200

    front-end setUp

---
+++ cloud_engine/src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div >
+        <p>Hello, this is Cloud Engine!</p>
+      </div>
+    </main>
+  )
+}
