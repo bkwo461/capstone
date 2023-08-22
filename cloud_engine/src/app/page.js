18a38519466cf7d4339ea82f5d30d5f696bc3c47
Sangjun Min <smin289@aucklanduni.ac.nz>
2023-08-22 22:44:14 +1200

    Top nav bar new design created

--- cloud_engine/src/app/page.js
+++ cloud_engine/src/app/page.js
@@ -6,7 +6,7 @@ import TopNav from './topNav';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{ backgroundColor: 'white' }}>
       <div className={`${styles.top_menu_bar} px-2 py-3`}>
         <h1 className="d-inline-block bold">Cloud Engine</h1>
       </div>
