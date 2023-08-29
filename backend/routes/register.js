a092d983750109ddc2157424d332a1f5eb5666d3
SGTkiwi <jhan327@aucklanduni.ac.nz>
2023-08-29 14:56:46 +1200

    connect db, sign up api test

---
+++ backend/routes/register.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const { User } = require('../models/accout');
+
+router.post("/", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    const userInfo = await user.save();
+    res.status(200).json({ success: true, userInfo });
+  } catch (err) {
+    res.json({ success: false, err });
+  }
+});
+
+
+module.exports = router;
