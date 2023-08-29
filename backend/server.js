1ea7cd76f5723312f7de289a44fb5af8dde99979
Mince Pie <29334216+521minsu@users.noreply.github.com>
2023-08-29 23:17:25 +1200

    Swagger UI Setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const connectMongoDB = require("./config/db.js");
-
 
 require("dotenv").config();
 
@@ -19,6 +21,38 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+
+// Swagger Initialization
+const swaggerOptions = {
+  swaggerDefinition: {
+      info: {
+          title: 'Cloud Engine API',
+          version: '0.0.1',
+          description: 'Welcome to Cloud Engine Backend API',
+      }
+  },
+  apis: ['./server.js', './routes/*.js', './routes/AWS/*.js']
+};
+
+swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// Swagger Routes
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.get('/swagger.json', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerDocs);
+});
+
+/**
+* @swagger
+* /:
+*  get:
+*    description: Sample endpoint to test if the server is running
+*    responses:
+*      '200':
+*        description: A successful response
+*
+*/
 app.get('/', (req, res) => {
     console.log('Hello World');
     res.status(200).send('Hello World');
@@ -35,10 +69,13 @@ const loginRoutes = require('./routes/login');
 const authRoutes = require('./routes/auth');
 const logoutRoutes = require('./routes/logout');
 
+// Route Definitions
 app.use('/api/cdk', cdkRoutes);
 app.use('/api/register', registerRoutes);
 app.use('/api/login', loginRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/logout', logoutRoutes);
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
