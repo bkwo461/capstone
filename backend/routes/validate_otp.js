c3e9547f2c8f2f6fdb72bcf9b1de6cacef5156ec
Sangjun Min <smin289@aucklanduni.ac.nz>
2023-09-03 20:47:30 +1200

    login api connect / register api connect

--- backend/routes/validate_otp.js
+++ backend/routes/validate_otp.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< Updated upstream
 const { OTP } = require('../models/otpModel'); // import your otp model
-=======
-const { OTP } = require('../models/OTPModel');
->>>>>>> Stashed changes
+
 
 router.post("/", async (req, res) => {
     const otpRecord = await OTP.findOne({ email: req.body.email });
