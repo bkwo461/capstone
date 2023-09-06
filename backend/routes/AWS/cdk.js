b9372f25894e89c587867df90743682ee7d8ba51
Mince Pie <29334216+521minsu@users.noreply.github.com>
2023-09-06 13:34:52 +1200

    async DONE w/ Promises

--- backend/routes/AWS/cdk.js
+++ backend/routes/AWS/cdk.js
@@ -17,7 +17,7 @@ AWS.config.update({
 AWS.config.getCredentials(function(err) {
     if (err) console.log(err.stack); // credentials not loaded
     else {
-      console.log("Access key:", AWS.config.credentials.accessKeyId);
+      console.log("ROOT Access key:", AWS.config.credentials.accessKeyId);
     }
   });
 
@@ -25,7 +25,7 @@ var iam = new AWS.IAM({apiVersion: '2010-05-08'});
 
 
 // Create User
-async function createAccessKey(params) {
+function createAccessKey(params) {
     let result = {
         user: null,
         accessKey: null,
@@ -34,20 +34,22 @@ async function createAccessKey(params) {
         message: null,
         rawErr: null,
     };
-    iam.createAccessKey(params, function (err, data) {
-        if (err) { // an error occurred  
-            if (debug) console.log(err, err.stack);
-            result.isError = true; result.message = 'Error creating IAM access key'; result.rawErr = err;
-            return result; 
-        } else { // successful response
-            if (debug) console.log(data);
-            if (debug) console.log(`Access Key ID: ${data.AccessKey.AccessKeyId} \nSecret Access Key: ${data.AccessKey.SecretAccessKey} \nUser ID: ${data.AccessKey.UserName}`);
-            result.user = data.AccessKey.UserName;
-            result.accessKey = data.AccessKey.AccessKeyId;
-            result.secretAccessKey = data.AccessKey.SecretAccessKey;
-            console.log(result);
-            return result;
-        }
+    return new Promise((resolve, reject) => {
+        iam.createAccessKey(params, function (err, data) {
+            if (err) { // an error occurred  
+                if (debug) console.log(err, err.stack);
+                result.isError = true; result.message = 'Error creating IAM access key'; result.rawErr = err;
+                resolve(result); 
+            } else { // successful response
+                if (debug) console.log(data);
+                if (debug) console.log(`Access Key ID: ${data.AccessKey.AccessKeyId} \nSecret Access Key: ${data.AccessKey.SecretAccessKey} \nUser ID: ${data.AccessKey.UserName}`);
+                result.user = data.AccessKey.UserName;
+                result.accessKey = data.AccessKey.AccessKeyId;
+                result.secretAccessKey = data.AccessKey.SecretAccessKey;
+                if (debug) console.log(result);
+                resolve(result);
+            }
+        });
     });
 }
 
@@ -73,7 +75,7 @@ function createPolicy(username, rdsLink) {
     return PolicyParams;
 }
 
-async function createFullUser(username, rdsLink) {
+function createFullUser(username, rdsLink) {
     const userparams = {
         UserName: username
     };
@@ -86,65 +88,66 @@ async function createFullUser(username, rdsLink) {
         message: null,
     };
 
-    iam.createPolicy(createPolicy(username, rdsLink), (err, policyData) => { // Create Policy
-        if (err) {
-            if (debug) console.error('Error creating IAM policy:', err);
-            result.isError = true; result.message = 'Error creating IAM policy'; result.rawErr = err;
-            return result;
-        } else {
-            if (debug) console.log('IAM policy created successfully:', policyData.Policy.PolicyName);
-        }
-
-        iam.getUser(userparams, function (err, data) { // Check if user already exists
-            if (err && err.code === 'NoSuchEntity') {   
-
-                iam.createUser(userparams, async function (err, data) {  // Create user
-                    if (err) {
-                        if (debug) console.log("Error creating IAM user:", err);
-                        result.isError = true; result.message = 'Error creating IAM user'; result.rawErr = err;
-                        return result;
-                    } else {
-                        if (debug) console.log("User Creation Success", data);
-
-                        
-                        if (debug) console.log(`policyData: ${policyData}`)
-
-                        const attachPolicyParams = {
-                            PolicyArn: policyData.Policy.Arn,
-                            UserName: data.User.UserName,
-                        };
-
-                        iam.attachUserPolicy(attachPolicyParams, (err, data) => {
-                            if (err) {
-                                if (debug) console.error('Error attaching policy to IAM user:', err);
-                                result.isError = true; result.message = 'Error attaching policy to IAM user'; result.rawErr = err;
-                                return result;
-                            } else {
-                                if (debug) console.log('Policy attached to IAM user successfully:', data);
-                            }
-                        });
-
-
-                        // Create Access Key
-                        result = await createAccessKey(userparams);
-                        return result;
-
-                    }
-                });
-
+    return new Promise((resolve, reject) => {
+        iam.createPolicy(createPolicy(username, rdsLink), (err, policyData) => { // Create Policy
+            if (err) {
+                if (debug) console.error('Error creating IAM policy:', err);
+                result.isError = true; result.message = 'Error creating IAM policy'; result.rawErr = err;
+                resolve(result);
             } else {
-                if (debug) console.log("User " + userparams.UserName + " already exists", data.User.UserId);
-                result.isError = true; result.message = `User ${userparams.UserName} already exists`; result.rawErr = err;
-                return result;
+                if (debug) console.log('IAM policy created successfully:', policyData.Policy.PolicyName);
             }
+
+            iam.getUser(userparams, function (err, data) { // Check if user already exists
+                if (err && err.code === 'NoSuchEntity') {   
+
+                    iam.createUser(userparams, async function (err, data) {  // Create user
+                        if (err) {
+                            if (debug) console.log("Error creating IAM user:", err);
+                            result.isError = true; result.message = 'Error creating IAM user'; result.rawErr = err;
+                            resolve(result);
+                        } else {
+                            if (debug) console.log("User Creation Success", data);
+
+                            
+                            if (debug) console.log(`policyData: ${policyData}`)
+
+                            const attachPolicyParams = {
+                                PolicyArn: policyData.Policy.Arn,
+                                UserName: data.User.UserName,
+                            };
+
+                            iam.attachUserPolicy(attachPolicyParams, (err, data) => {
+                                if (err) {
+                                    if (debug) console.error('Error attaching policy to IAM user:', err);
+                                    result.isError = true; result.message = 'Error attaching policy to IAM user'; result.rawErr = err;
+                                    resolve(result);
+                                } else {
+                                    if (debug) console.log('Policy attached to IAM user successfully:', data);
+                                }
+                            });
+
+
+                            // Create Access Key
+                            createAccessKey(userparams).then(data => {resolve(data);}).catch(err => {console.log(err);});
+
+                        }
+                    });
+
+                } else {
+                    if (debug) console.log("User " + userparams.UserName + " already exists", data.User.UserId);
+                    result.isError = true; result.message = `User ${userparams.UserName} already exists`; result.rawErr = err;
+                    resolve(result);
+                }
+            });
         });
     });
 }
 
 
 async function main() {
-    let result = await createFullUser('testuser6');
-    console.log(result);
+    // console.log(createFullUser('testuser8'));
+    createFullUser('testuser9').then(data => {console.log(data);}).catch(err => {console.log(err);});
 }
 
 main();
