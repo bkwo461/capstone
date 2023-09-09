67ffe589dd7a8791a124aeb3fa0ecca7a59310c6
Mince Pie <29334216+521minsu@users.noreply.github.com>
2023-09-09 23:25:25 +1200

    Auth-related pages Drafted

--- newfrontend/routes/auth.js
+++ newfrontend/routes/auth.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('auth/auth.html', { title: 'Express' });
+
+router.get('/login', function(req, res, next) {
+  res.render('auth/login.html', { title: 'Express' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('auth/signup.html', { title: 'Express' });
+});
+
+router.get('/forgot', function(req, res, next) {
+  res.render('auth/forgot.html', { title: 'Express' });
 });
 
 module.exports = router;
